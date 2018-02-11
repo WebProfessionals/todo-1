c97acef1d8c3d5e01d45f8ab70b82b5120b0e6a5
Livia Kobler <livia.kobler@bluewin.ch>
2018-02-11 09:09:58 +0100

    test eingerichten

--- js/test.js
+++ js/test.js
@@ -1,5 +1,17 @@
-it('double done', function(done) {
-    // Calling `done()` twice is an error
-    setImmediate(done);
-    setImmediate(done);
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+
+var Task = require('./todo.js').Task;
+var TodoList = require('./todo.js').TodoList;
+
+describe('Todo', function () {
+
+    describe('Task', function () {
+
+    });
+
+    describe('TodoList', function () {
+
+    });
 });
+
---
+++ js/todo.js
@@ -0,0 +1,22 @@
+"use strict";
+
+class Task {
+
+}
+
+class TodoList {
+
+}
+
+
+
+
+
+
+
+
+// Machen wir nur für die Tests
+if (typeof module !== 'undefined' && module) {
+    module.exports.Task = Task;
+    module.exports.TodoList = TodoList;
+}
