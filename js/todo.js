461a93eba36e7ed7588cc2b6fad67ce53c027b2e
veith <veithz@gmail.com>
2018-02-23 21:14:31 +0100

    check uncheck delete auf UI

--- js/todo.js
+++ js/todo.js
@@ -9,12 +9,19 @@ ready(() => {
 
   let taskInputFields = document.querySelectorAll('.todo__inputfield');
   let liste = document.getElementById('liste');
+  let erledigtliste = document.getElementById('erledigtliste');
 
+
+  // listener für Task Aktionen
   liste.addEventListener('click', e => {
-
-    console.dir(e.target)
-
+    loeschenWennMoeglich(e);
+    erledigenWennMoeglich(e);
   });
+  erledigtliste.addEventListener('click', e => {
+    loeschenWennMoeglich(e);
+    unerledigenWennMoeglich(e);
+  });
+
 
   taskInputFields.forEach((inputEl) => {
     // Auf Enter Taste hören
@@ -63,19 +70,26 @@ ready(() => {
     item.task = task;
 
     let del = document.createElement('button');
-    del.innerText='del';
+    del.innerText = 'del';
     del.classList.add('delete');
     item.appendChild(del);
 
     let erledigt = document.createElement('button');
-    erledigt.innerText='done';
-    erledigt.classList.add('erledigt');
-    item.appendChild(erledigt);
+    if (!task.erledigt) {
+      erledigt.innerText = '[ ]';
+      erledigt.classList.add('check');
+      item.appendChild(erledigt);
+    } else {
+      erledigt.innerText = '[x]';
+      erledigt.classList.add('uncheck');
+      item.appendChild(erledigt);
+    }
 
     item.classList.add('todo__item');
     let text = document.createElement('text');
     text.innerText = task.text;
     item.appendChild(text);
+
 
     return item;
   };
@@ -85,4 +99,42 @@ ready(() => {
   liste.innerHTML = '';
   initTaskList(todoList.tasks);
 
+
+  /**
+   * prüft ob ein Eintrag als erledigt markiert werden kann,
+   * und macht es dann auch gleich
+   * @param e
+   */
+  let erledigenWennMoeglich = function (e) {
+    if (e.target.classList.contains('check')) {
+      e.target.parentNode.task.check();
+      erledigtliste.appendChild(createItemDom(e.target.parentNode.task));
+      e.target.parentNode.remove();
+    }
+  };
+
+  /**
+   * Prüft ob ein Eintrag gelöscht werden soll
+   * und macht es dann auch gleich
+   * @param e
+   */
+  let loeschenWennMoeglich = function (e) {
+    if (e.target.classList.contains('delete')) {
+      todoList.removeTaskByID(e.target.parentNode.task.id);
+      e.target.parentNode.remove();
+    }
+  };
+
+  /**
+   * prüft ob ein Eintrag als unerledigt markiert werden kann,
+   * und macht es dann auch gleich
+   * @param e
+   */
+  let unerledigenWennMoeglich = function (e) {
+    if (e.target.classList.contains('uncheck')) {
+      e.target.parentNode.task.uncheck();
+      liste.appendChild(createItemDom(e.target.parentNode.task));
+      e.target.parentNode.remove();
+    }
+  };
 });
