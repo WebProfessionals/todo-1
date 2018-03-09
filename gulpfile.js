39d061da55d9d51ffbdf3adfd584abe0f57b12d8
veith <veithz@gmail.com>
2018-03-09 20:53:26 +0100

    babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
 
+gulp.task('babel', () =>
+    gulp.src('src/js/*.js')
+        .pipe(babel({
+          presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('dist'))
+);
 
 gulp.task('hello', () => {
   console.log('Hello World');
 });
 
-var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./src/scss/**/*.scss')
+      .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest('./src/css'));
 });
 
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/scss/**/*.scss', ['sass']);
+});
