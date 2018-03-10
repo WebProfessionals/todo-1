707f04325d9270c1cccb0686366823e213877d6f
veith <veithz@gmail.com>
2018-03-10 09:16:45 +0100

    Browsersync eingebaut

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,20 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
+const clean = require('gulp-clean');
 
-gulp.task('build',['babel','sass','copy','assets']);
+const browserSync = require('browser-sync').create();
 
+gulp.task('build', ['clean', 'babel', 'sass', 'copy']);
+
+
+gulp.task('clean', function () {
+  return gulp.src('dist', {read: false})
+      .pipe(clean());
+});
 
 gulp.task('babel', () =>
-    gulp.src(['src/js/*.js','src/node_modules/todooo/todo.js'], {base:'src/'})
+    gulp.src(['src/js/*.js', 'src/node_modules/todooo/todo.js'], {base: 'src/'})
         .pipe(babel({
           presets: ['env']
         }))
@@ -20,21 +28,28 @@ gulp.task('sass', function () {
       .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
       .pipe(sourcemaps.write())
-      .pipe(gulp.dest('./src/css'));
+      .pipe(gulp.dest('./src/css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
 });
 
 
 gulp.task('copy', function () {
   gulp
-      .src(['src/index.html', 'src/**/*.css'])
+      .src(['src/index.html', 'src/**/*.css', 'src/media/**/*.svg'], {base: 'src/'})
       .pipe(gulp.dest('dist'));
 });
 
-gulp.task('assets', function () {
-  gulp
-      .src(['src/media/**/*.svg'])
-      .pipe(gulp.dest('dist/media'));
+gulp.task('browserSync', function () {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+  })
 });
-gulp.task('sass:watch', function () {
+
+
+gulp.task('sass:watch', ['browserSync'], function () {
   gulp.watch('./src/scss/**/*.scss', ['sass']);
 });
